c3801bee79310aff03a81091a989ac6e7333ee5c
Wellend Phegyansen <wellendphegyansen@gmail.com>
2024-05-29 16:23:38 +0700

    Add files via upload

---
+++ SE.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	int b;
+	
+	printf("Input number 1: ");
+	scanf("%d", &a);
+	
+	getchar();
+	printf("Input number 2: ");
+	scanf("%d", &b);
+	
+	printf("Result: %d\n", a+b);
+}
+
